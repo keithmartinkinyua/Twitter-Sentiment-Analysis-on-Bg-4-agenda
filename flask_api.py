85060f27903b6507e8d116a5ce91a53f6294a7a1
Rodney Osodo <blackd0t@protonmail.com>
2019-04-05 02:59:24 +0300

    Adding flask api

---
+++ flask_api.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from Scripts import sentiment as s
+
+api = Flask(__name__)
+@api.route("/")
+def index():
+	return jsonify({
+		"data": "Success"
+		})
+
+@api.route("/tweet/<message>", methods=["GET", "PUT"])
+def data(message):
+	sent = s.sentiment(str(message))
+	if sent[0] == "pos":
+		return jsonify({
+			"message" : message,
+			"response": "The message is positive with a confidence of {}".format(sent[1] * 100)
+			})
+	elif sent[0] == "neg":
+		return jsonify({
+			"message" : message,
+			"response": "The message is negative with a confidence of {}".format(sent[1] * 100)
+			})
+
+
+if __name__ == '__main__':
+    api.run(debug=True)
